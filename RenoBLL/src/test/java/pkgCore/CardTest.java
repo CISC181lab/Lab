be4f851e7e1c8f9149fdf2a285f5f9fd7d2fe5a1
michalbargenda <michalbargenda@wifi-roaming-128-4-135-184.host.udel.edu>
2020-02-17 16:20:24 -0500

    Tests

--- RenoBLL/src/test/java/pkgCore/CardTest.java
+++ RenoBLL/src/test/java/pkgCore/CardTest.java
@@ -20,14 +20,14 @@ public class CardTest {
 	 *        Card_Create_Test - Test to make sure constructor works, getter/setter works
 	 */
 	
-//	@Test
-//	public void Card_Create_Test() {
-//		Card c = new Card(eSuit.CLUBS, eRank.EIGHT);
-//		assertNotNull(c);
-//		assertEquals(eSuit.CLUBS, c.geteSuit());
-//		assertEquals(eRank.EIGHT, c.geteRank());
-//		assertFalse(c.isWild());
-//	}
+	@Test
+	public void Card_Create_Test() {
+		Card c = new Card(eSuit.CLUBS, eRank.EIGHT);
+		assertNotNull(c);
+		assertEquals(eSuit.CLUBS, c.geteSuit());
+		assertEquals(eRank.EIGHT, c.geteRank());
+		assertFalse(c.isWild());
+	}
 
 	/**
 	 * @author BRG
@@ -36,16 +36,16 @@ public class CardTest {
 	 * 
 	 *        Card_Equal_Test - test to make sure the equals method works
 	 */
-//	@Test
-//	public void Card_Equal_Test() {
-//		Card c1 = new Card(eSuit.HEARTS, eRank.TWO);
-//		Card c2 = new Card(eSuit.HEARTS, eRank.TWO);
-//
-//		assertTrue(c1.equals(c2));
-//
-//		Card c3 = new Card(eSuit.HEARTS, eRank.THREE);
-//		assertFalse(c1.equals(c3));
-//	}
+	@Test
+	public void Card_Equal_Test() {
+		Card c1 = new Card(eSuit.HEARTS, eRank.TWO);
+		Card c2 = new Card(eSuit.HEARTS, eRank.TWO);
+
+		assertTrue(c1.equals(c2));
+
+		Card c3 = new Card(eSuit.HEARTS, eRank.THREE);
+		assertFalse(c1.equals(c3));
+	}
 
 	/**
 	 * @author BRG
@@ -54,23 +54,23 @@ public class CardTest {
 	 * 
 	 *        Card_Sort_Test - test to make sure the card sorting works
 	 */
-//	@Test
-//	public void Card_Sort_Test() {
-//
-//		ArrayList<Card> cards = new ArrayList<Card>();
-//		Card c1 = new Card(eSuit.CLUBS, eRank.TWO);
-//		Card c2 = new Card(eSuit.CLUBS, eRank.EIGHT);
-//		Card c3 = new Card(eSuit.DIAMONDS, eRank.SIX);
-//
-//		cards.add(c1);
-//		cards.add(c2);
-//		cards.add(c3);
-//
-//		Collections.sort(cards);
-//
-//		assertEquals(cards.get(0).geteRank(), eRank.EIGHT);
-//		assertEquals(cards.get(1).geteRank(), eRank.SIX);
-//		assertEquals(cards.get(2).geteRank(), eRank.TWO);
-//	}
+	@Test
+	public void Card_Sort_Test() {
+
+		ArrayList<Card> cards = new ArrayList<Card>();
+		Card c1 = new Card(eSuit.CLUBS, eRank.TWO);
+		Card c2 = new Card(eSuit.CLUBS, eRank.EIGHT);
+		Card c3 = new Card(eSuit.DIAMONDS, eRank.SIX);
+
+		cards.add(c1);
+		cards.add(c2);
+		cards.add(c3);
+
+		Collections.sort(cards);
+
+		assertEquals(cards.get(0).geteRank(), eRank.EIGHT);
+		assertEquals(cards.get(1).geteRank(), eRank.SIX);
+		assertEquals(cards.get(2).geteRank(), eRank.TWO);
+	}
 
 }
